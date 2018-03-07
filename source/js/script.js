7bc8d8386d01510cf679d2dde0cac42d54e69191
mglushakova <glushakova7@mail.ru>
2018-03-07 16:16:01 +0500

    Добавляет скрипт переключения списков подарков

--- source/js/script.js
+++ source/js/script.js
@@ -15,3 +15,31 @@ menuButton.addEventListener("click", function (event) {
     menu.classList.remove("main-nav--opened");
   }
 });
+
+//Prize category open
+var bookedButton = document.querySelector(".prize__status--booked");
+var availableButton = document.querySelector(".prize__status--available");
+var bookedList = document.querySelector(".prize__category--booked");
+var availableList = document.querySelector(".prize__category--available");
+
+bookedButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if ( !bookedButton.classList.contains("prize__status--active") ) {
+    bookedButton.classList.add("prize__status--active");
+    availableButton.classList.remove("prize__status--active");
+    bookedList.classList.add("prize__category--active");
+    availableList.classList.remove("prize__category--active");
+  }
+});
+
+availableButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  if ( !availableButton.classList.contains("prize__status--active") ) {
+    availableButton.classList.add("prize__status--active");
+    bookedButton.classList.remove("prize__status--active");
+    availableList.classList.add("prize__category--active");
+    bookedList.classList.remove("prize__category--active");
+  }
+});
